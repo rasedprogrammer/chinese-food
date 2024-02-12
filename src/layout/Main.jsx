21fd4cc877ed52072892419cc20ed12bd7cb9316
rasedprogrammer <rasedprogrammer@gmail.com>
2024-02-12 12:55:54 +0600

    [ADD] header and footer

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,13 +1,15 @@
-import {Outlet} from 'react-router-dom';
-import Footer from '../Shared/Footer/Footer';
+import { Outlet } from "react-router-dom";
+import Footer from "../Shared/Footer/Footer";
+import NavBar from "../Shared/NavBar/NavBar";
 
 const Main = () => {
-    return (
-        <div>
-            <Outlet></Outlet>
-            <Footer></Footer>
-        </div>
-    );
+  return (
+    <div>
+      <NavBar></NavBar>
+      <Outlet></Outlet>
+      <Footer></Footer>
+    </div>
+  );
 };
 
 export default Main;
