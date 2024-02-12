f7f98e6cddcba2a36d27a9c48da697b84a292cba
rasedprogrammer <rasedprogrammer@gmail.com>
2024-02-12 13:29:14 +0600

    [Adding slider banner section]

--- src/Shared/NavBar/NavBar.jsx
+++ src/Shared/NavBar/NavBar.jsx
@@ -23,7 +23,7 @@ const NavBar = () => {
     </>
   );
   return (
-    <div className="navbar bg-base-100">
+    <div className="navbar fixed z-10 max-w-screen-xl bg-opacity-30 bg-black text-white">
       <div className="navbar-start">
         <div className="dropdown">
           <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
